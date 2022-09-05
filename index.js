85dc56b9768a4f376b671143e02fd7aecf532db2
Meridith Lawn <meridithlawn2@gmail.com>
2022-09-05 18:08:44 -0500

    commit tree 92477160f85cdeb3b290c946eb75e6889c8010de

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield",];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo");
+}
+function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+function prependCat(name) {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+function removeLastCat() {
+    const newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = cats.slice();
+    newCats.shift();
+    return newCats;
+}
